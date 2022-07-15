bffb7970fa6dc6e13d93fc68d5a49554a4b67d60
sigkill <blakat360@gmail.com>
2022-07-15 03:00:32 +0100

    added startx reqs

---
+++ x/.screenlayout/work_room.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 1024x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1280x1024 --pos 0x181 --rotate left --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --mode 1280x1024 --pos 3584x181 --rotate left
